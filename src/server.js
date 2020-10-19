1f27ab139a28babf57d07791dac2e6f017a0e398
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-20 00:32:39 +0530

    API: Apollo server & Database connection

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("apollo-server");
+const { sequelize } = require("../models");
+
+const typeDefs = require("./graphql/type-defs");
+const resolvers = require("./graphql/resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
